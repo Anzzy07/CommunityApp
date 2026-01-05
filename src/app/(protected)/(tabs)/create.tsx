97772b185023243f27d85dd06cb50e44fbd7c1e3
Anzzy07 <anzel.acharya307@gmail.com>
2026-01-05 08:06:56 +0545

    Create screen with community selector. Added Jotai as global state management.

--- src/app/(protected)/(tabs)/create.tsx
+++ src/app/(protected)/(tabs)/create.tsx
@@ -1,8 +1,11 @@
+import { selectedGroupAtom } from "@/src/atoms";
 import { COLORS } from "@/src/colors";
 import { AntDesign } from "@expo/vector-icons";
 import { Link, router } from "expo-router";
+import { useAtom } from "jotai";
 import React, { useState } from "react";
 import {
+  Image,
   KeyboardAvoidingView,
   Platform,
   Pressable,
@@ -17,10 +20,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function CreateScreen() {
   const [title, setTitle] = useState<string>("");
   const [bodyText, setbodyText] = useState<string>("");
+  const [group, setGroup] = useAtom(selectedGroupAtom);
 
   const goBack = () => {
     setTitle("");
     setbodyText("");
+    setGroup(null);
     router.back();
   };
   return (
@@ -28,17 +33,23 @@ export default function CreateScreen() {
       style={{ backgroundColor: COLORS.border, flex: 1, paddingHorizontal: 10 }}
     >
       {/* Header */}
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Pressable onPress={() => goBack()} style={{ marginLeft: "auto" }}>
-          <AntDesign
-            name="close"
-            size={30}
-            color={COLORS.primaryDark}
-            onPress={() => router.back()}
-          />
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: 44,
+        }}
+      >
+        <Pressable onPress={goBack} hitSlop={10}>
+          <AntDesign name="close" size={30} color={COLORS.primaryDark} />
+        </Pressable>
+
+        <Pressable onPress={() => console.log("POST")} hitSlop={10}>
           <Text style={styles.postText}>Post</Text>
         </Pressable>
       </View>
+
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : undefined}
         style={{ flex: 1 }}
@@ -47,8 +58,20 @@ export default function CreateScreen() {
           {/* Community Selector */}
           <Link href={"groupSelector"} asChild>
             <Pressable style={styles.communityContainer}>
-              <Text style={styles.kStyles}>k/</Text>
-              <Text style={{ fontWeight: "600" }}>Select a community</Text>
+              {group ? (
+                <>
+                  <Image
+                    source={{ uri: group.image }}
+                    style={{ width: 20, height: 20, borderRadius: 10 }}
+                  />
+                  <Text style={{ fontWeight: "600" }}>{group.name}</Text>
+                </>
+              ) : (
+                <>
+                  <Text style={styles.kStyles}>k/</Text>
+                  <Text style={{ fontWeight: "600" }}>Select a community</Text>
+                </>
+              )}
             </Pressable>
           </Link>
 
@@ -82,8 +105,9 @@ const styles = StyleSheet.create({
     color: "white",
     backgroundColor: COLORS.success,
     fontWeight: "bold",
-    paddingHorizontal: 5,
-    paddingVertical: 2,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 15,
   },
   kStyles: {
     backgroundColor: "black",
--- src/app/(protected)/groupSelector.tsx
+++ src/app/(protected)/groupSelector.tsx
@@ -1,21 +1,118 @@
+import groups from "@/assets/data/groups.json";
+import { selectedGroupAtom } from "@/src/atoms";
 import { COLORS } from "@/src/colors";
-import { AntDesign } from "@expo/vector-icons";
+import { Group } from "@/src/types";
+import { AntDesign, EvilIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import React from "react";
-import { View } from "react-native";
+import { useSetAtom } from "jotai";
+import React, { useState } from "react";
+import {
+  FlatList,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function GroupSelector() {
+  const [searchValue, setSearchValue] = useState<string>("");
+  const setGroup = useSetAtom(selectedGroupAtom);
+
+  const filterGroups = groups.filter((group) =>
+    group.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const onGroupSelected = (group: Group) => {
+    setGroup(group);
+    router.back();
+  };
+
   return (
-    <SafeAreaView>
-      <View>
-        <AntDesign
-          name="close"
-          size={30}
-          color={COLORS.primaryDark}
-          onPress={() => router.back()}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1, backgroundColor: "white", paddingHorizontal: 10 }}
+    >
+      <SafeAreaView style={{ marginHorizontal: 10, flex: 1 }}>
+        <View
+          style={{
+            height: 44,
+            justifyContent: "center",
+          }}
+        >
+          <AntDesign
+            name="close"
+            size={30}
+            color={COLORS.primaryDark}
+            onPress={() => router.back()}
+            style={{ position: "absolute", left: 0 }}
+          />
+
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Post to
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "lightgrey",
+            borderRadius: 5,
+            gap: 5,
+            marginVertical: 10,
+            alignItems: "center",
+            paddingHorizontal: 5,
+          }}
+        >
+          <EvilIcons name="search" size={16} color="black" />
+          <TextInput
+            placeholder="Search for the community"
+            placeholderTextColor={COLORS.primaryDark}
+            style={{ paddingVertical: 10, flex: 1 }}
+            value={searchValue}
+            onChangeText={(text) => setSearchValue(text)}
+          />
+          {searchValue && (
+            <AntDesign
+              name="close-circle"
+              size={15}
+              color="#E4E4E4"
+              onPress={() => setSearchValue("")}
+            />
+          )}
+        </View>
+
+        <FlatList
+          data={filterGroups}
+          keyboardShouldPersistTaps="handled"
+          renderItem={({ item }) => (
+            <Pressable
+              onPress={() => onGroupSelected(item)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                marginBottom: 20,
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 40, aspectRatio: 1, borderRadius: 20 }}
+              />
+              <Text style={{ fontWeight: "600" }}>{item.name}</Text>
+            </Pressable>
+          )}
         />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
